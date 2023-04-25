42e679c49d441638b06b6626f079461ab479d64d
Behzod <bmoydinboyev@usf.edu>
2023-04-25 15:44:01 -0400

    add non-functional post preview

--- src/Posts.js
+++ src/Posts.js
@@ -1,29 +1,37 @@
 import { useParams, useNavigate } from "react-router-dom";
 import NavBar from "./components/NavBar";
-import { Grid } from "@chakra-ui/react";
+import { Grid, SimpleGrid, Heading } from "@chakra-ui/react";
+import SearchBar from "./components/SearchBar";
+import { useEffect, useState } from "react";
+import PostPreview from "./components/PostPreview";
 
 const Posts = ({userId}) => {
     // don't change the name, id, because it's used in the router
     const { id } = useParams();
     const navigate = useNavigate();
-    // if id is null or doesn't exist, navigate to 404 page
-    // TODO: actually check the database to see if the course id exists
-    if (!id) {   
-        navigate('/404', {message: 'Course ID is not found'});
-        return null;
-    }
 
-    // TODO: fetch posts from backend http://localhost:5000/classes/${id}/posts`
-    
+    const [posts, setPosts] = useState([]);
 
-
+    useEffect(() => {
+        async function fetchData() {
+            const response = await fetch(`http://localhost:5000/classes/${id}/posts`);
+            const data = await response.json();
+            setPosts(data);
+        } fetchData();
+    }, [id]);
 
     return (
-        <Grid justifyContent={'center'}>
+        <Grid justifyContent={'center'} mb={100}>
             {/*Pass course ID because the saved button 
             in Navbar returns saved posts of the specific course*/}
             <NavBar userId={userId} courseID={id}/>
-            <h1>Posts here for course {id}</h1>
+            <SearchBar/>
+            <Heading size='lg' mb={50} textAlign={'center'}>Posts in Course {id}</Heading>
+            <SimpleGrid spacing={5} templateColumns='repeat(auto-fill, minmax(250px, 1fr))'>
+                {posts.map((dataItem, index) => (
+                    <PostPreview key={index} data={dataItem} />
+                ))}
+            </SimpleGrid>
         </Grid>
     );
 }
