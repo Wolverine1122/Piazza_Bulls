b7cc47d5b190f287bcff202d2c8b99b89980dc12
R-Persaud <renukapersaud1@gmail.com>
2023-04-26 13:38:19 -0400

    login incorrect credentials handling

--- src/LogIn.js
+++ src/LogIn.js
@@ -5,9 +5,10 @@ export const LogIn = ({onLogin}) => {
     const [username, setUsername] = useState('');
     const [pass, setPass] = useState('');
     const navigate = useNavigate();
+    const [errors, setErrors] = useState();
    
    //connect here <- currently just logging it 
-   const handleSubmit =  (e) => 
+   const handleSubmit = async (e) => 
    {
     const body = {
         username: username,
@@ -16,19 +17,24 @@ export const LogIn = ({onLogin}) => {
 
     e.preventDefault();
   
-    const response = fetch('http://localhost:5000/login', {
+    const response = await fetch('http://localhost:5000/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(body)
         })
-        .then((response) => {
+       // .then((response) => {
             if (response.ok) {
                 onLogin(username);
                 navigate(`/courses/${username}`); // redirect to courses page
             }
-        });
+            else
+            {
+                const data = await response.json();
+                setErrors(data.error);
+            }
+       // });
    }
 
     return (
@@ -36,12 +42,15 @@ export const LogIn = ({onLogin}) => {
         <div className = "w-96 p-6 shadow-lg bg-white rounded-xl">
             <h2 className="text-2xl block text-center font-semibold">Login Here</h2>
         <form  className= "" onSubmit ={handleSubmit}>
+            
             <label htmlFor="username" className="block text-base mb-2">Username</label>
             <input value={username} onChange={(e)=>setUsername(e.target.value)} type="username" placeholder="username" id="username" name="username" className="bg-white-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"/>
             
             <label  htmlFor="password" className="block text-base mt-2 mb-2">Password</label>
             <input value={pass} onChange={(e)=>setPass(e.target.value)} type="password" placeholder="password" id="password" name="password" className="bg-white-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"/>
+            {errors && <span className="text-red-500">{errors}</span>}
             <button  type="submit" className=" mt-5 border-2 border-indigo-700 bg-indigo-700 text-white py-1 w-full rounded-lg hover:bg-indigo-500 hover:text-white font-semibold ">Login</button>
+            
        </form>
        <div className="p-5 text-center font-medium text-black hover:underline dark:text-primary-500">
        <Link to="/sign-up" >Don't have an account? Sign up here</Link>
