e8d900b519d62193f0ab81cd250d7cc70191d2a8
Behzod <bmoydinboyev@usf.edu>
2023-04-28 13:10:04 -0400

    add unstyled full post card function

--- src/Posts.js
+++ src/Posts.js
@@ -1,9 +1,10 @@
 import { useParams, useNavigate } from "react-router-dom";
 import NavBar from "./components/NavBar";
-import { Grid, SimpleGrid, Heading } from "@chakra-ui/react";
+import { Grid, SimpleGrid, Heading, useDisclosure } from "@chakra-ui/react";
 import SearchBar from "./components/SearchBar";
 import { useEffect, useState } from "react";
 import PostPreview from "./components/PostPreview";
+import Post from "./components/Post";
 
 const Posts = ({userId}) => {
     // don't change the name, id, because it's used in the router
@@ -11,6 +12,8 @@ const Posts = ({userId}) => {
     const navigate = useNavigate();
 
     const [posts, setPosts] = useState([]);
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const [selectedCard, setSelectedCard] = useState(null);
 
     useEffect(() => {
         async function fetchData() {
@@ -19,6 +22,18 @@ const Posts = ({userId}) => {
             setPosts(data);
         } fetchData();
     }, [id]);
+
+
+
+    const handleViewClick = (card) => {
+        setSelectedCard(card);
+        onOpen();
+      };
+
+    const handleCloseClick = () => {
+        setSelectedCard(null);
+        onClose();
+    };
 
     return (
         <Grid justifyContent={'center'} mb={100}>
@@ -29,9 +44,10 @@ const Posts = ({userId}) => {
             <Heading size='lg' mb={50} textAlign={'center'}>Posts in Course {id}</Heading>
             <SimpleGrid spacing={5} templateColumns='repeat(auto-fill, minmax(300px, 1fr))'>
                 {posts.map((dataItem, index) => (
-                    <PostPreview key={index} data={dataItem} />
+                    <PostPreview key={index} data={dataItem} onViewClick={() => handleViewClick(dataItem)}/>
                 ))}
             </SimpleGrid>
+            {selectedCard !== null && <Post isOpen={isOpen} onClose={handleCloseClick} data={selectedCard}></Post>}
         </Grid>
     );
 }
--- src/components/PostPreview.js
+++ src/components/PostPreview.js
@@ -1,11 +1,12 @@
 import {Card, CardHeader, Heading, CardBody, CardFooter, Text, Grid, Tag, Button, GridItem} from '@chakra-ui/react';
-import {ViewIcon, ChatIcon, ChevronUpIcon, ChevronDownIcon} from '@chakra-ui/icons';
+import {ViewIcon, DownloadIcon, ChevronUpIcon, ChevronDownIcon} from '@chakra-ui/icons';
 import TimeTag from './TimeTag';
 
-const PostPreview = (props) => {    
+const PostPreview = ({data, onViewClick}) => {    
+    const {title, description, username, activitytag, datecreated, totalvote} = data;
 
     let tagColor = '';
-    switch(props.data.activitytag) {
+    switch(activitytag) {
         case 'question':
             tagColor = 'red';
             break;
@@ -22,34 +23,33 @@ const PostPreview = (props) => {
     return (
         <Card>
             <CardHeader>
-                <Heading size='sm'>{props.data.title}</Heading>
+                <Heading size='sm'>{title}</Heading>
             </CardHeader>
             <CardBody>
-                <Text>{props.data.description}</Text>
+                <Text>{description}</Text>
             </CardBody>
             <CardFooter>
                 <Grid minW={'100%'}>
                     <Grid gap={'1rem'} templateColumns={'repeat(3, max-content)'} mb={5} justifyContent={'space-between'}>
-                        <Tag>{props.data.username}</Tag>
-                        <Tag colorScheme={tagColor}>{props.data.activitytag}</Tag>
-                        <TimeTag data={props.data.datecreated}/>
+                        <Tag>{username}</Tag>
+                        <Tag colorScheme={tagColor}>{activitytag}</Tag>
+                        <TimeTag data={datecreated}/>
                     </Grid>
-                    <Grid gap={'3'} templateColumns={'1fr max-content'}>
-                        <Grid gap={2} templateColumns={'1fr 1fr'}>
-                            <Button flex='1' variant='ghost' iconSpacing='5px' leftIcon={<ViewIcon />}>View</Button>
-                            <Button flex='1' variant='ghost' iconSpacing='5px' leftIcon={<ChatIcon />}>Comment</Button>
-                            
-                        </Grid>
+                    <Grid gap={'3'} templateColumns={'max-content 1fr'}>
                         <Grid templateColumns={'1.5em max-content 1.5em'} alignContent='center' justifyContent={'space-between'}>
                             <GridItem>
                                 <ChevronUpIcon color='red' boxSize='1.5em'/>
                             </GridItem>
                             <GridItem>
-                                <p>{props.data.totalvote}</p>
+                                <p>{totalvote}</p>
                             </GridItem>
                             <GridItem>
                                 <ChevronDownIcon color='blue' boxSize='1.5em'/>
                             </GridItem>
+                        </Grid>
+                        <Grid gap={2} templateColumns={'1fr 1fr'}>
+                            <Button flex='1' variant='ghost' iconSpacing='5px' leftIcon={<ViewIcon />} onClick={onViewClick}>View</Button>
+                            <Button flex='1' variant='ghost' iconSpacing='5px' leftIcon={<DownloadIcon />}>Save</Button>
                         </Grid>
                     </Grid>
                 </Grid>
